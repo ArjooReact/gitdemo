aab355313a13250dc1265412426835fe3539273d
ArjooReact <electronicsarzoo@gmail.com>
2024-06-09 13:08:29 +0530

    added new line for git push checking

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React Arjoo
+          Learn React Arjoo added new code in master
         </a>
       </header>
     </div>
